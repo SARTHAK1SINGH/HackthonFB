8d6f5aa8f6e17e32dd4dc9497cd5d9448c494854
SARTHAK1SINGH <44469087+SARTHAK1SINGH@users.noreply.github.com>
2021-03-02 20:23:22 +0530

    Marchupdates

---
+++ HackthonFB/README.md
@@ -0,0 +1,131 @@
+# HackthonFB
+
+## Requirements
+**Install these using**  ```pip install```
+
+> Python3 <br>
+> Flask==1.0.2 <br>
+> Flask-SQLAlchemy==2.4.0 <br>
+> pandas <br>
+> Flask-Dance <br>
+> wit <br>
+> newsapi-python==0.2.6 <br>
+> tweepy <br>
+> tweet-preprocessor 
+<hr><hr>
+
+## To Run Server 
+```python -m flask run```  <hr><hr>
+
+## See Live on
+<a href="https://curemate.herokuapp.com/" target="_blank">Click here: Cure-Mate</a>
+
+<hr><hr>
+
+
+![Annotation 2020-09-06 153803](https://user-images.githubusercontent.com/44469087/92323506-03839f00-f057-11ea-9cc3-c631f5cffafa.png)
+
+
+# Cure-Mate 🏥
+
+### Sentiment analysis of user reviews for particular disease and medicine Using Wit.Ai with Covid-19 Chatbot <br><br>
+We are trying to give the user fair information about what others think about that medicine he is using and what other <br> best-rated medicines suggested for this particular disease and User can also compare two Medicines to know the best one.<br>
+We are using the UCI drug review dataset for getting reviews having more than 2 million reviews.<br>
+We have implemented a chatbot for all your queries on Covid-19 and it works on both **Messenger** and **Telegram**.
+
+<hr><hr>
+
+# Aim 🎯 <br>
+Our aim is to give a platform that will be useful for both medical professionals and other Users. On this platform, they can study their situation and medicines, they can easily see the reviews for the work of medicine in that condition, and by our monitoring tool, they can get the latest information about the disease and its medicine. It would be really helpful in these pandemic situations like Covid-19 created. These situations create shortages of medicines. So, through our platform medical professionals and users can easily check alternate best medicine for the same situation and much other information that they need.
+
+<hr><hr>
+
+# Features 🌟
+
+### ▶️Search Box <br>
+⌨️ 1.**Typing Search**  <br>
+You can normally search through our search box and Suggestions will be provided beneath it for better help to get your desired name.<br>
+🎙️ 2.**Voice Search** <br> 
+An interactive Voice search option is also available. Just click on the MIC icon to trigger voice search and an interactive modulated voice will help you throughout your search. <br>
+### ▶️ Search From<br>
+At the bottom of the search box, there is an option of selecting a relevant source **( Ex. UCI Dataset, Twitter, Drug.Com(WIP) and other NewsAPI )** from where data is taken and then broken into tokens and Using WIT.AI inbuilt   NLP and Sentiment Analysis the Sentiment is negative, positive or neutral is taken out and displayed in results.<br>
+### ▶️ Sentiment Analysis <br>
+We get the relevant data from the user-desired platform and then tokens are generated from the data then Using NLP, Sentiment Analysis with the help of Wit.Ai we get the sentiment in each data and the model predicts the average sentiment of all persons and displays them in results.  <br>
+### ▶️ Results <br>
+Using Sentiment analysis the results for the medicine and disease are shown as written in the previous section.
+Now, in results, we also display other medicines that are rated good for the same disease and the other disease that can be cured by the same medicine. You can also download the result as a pdf softcopy.<br>
+We also display the Graph to better understand both **Sentiment Analysis** results and other **best-rated medicines.**
+![Annotation 2020-08-30 172213](https://user-images.githubusercontent.com/44469087/91658394-733cdb80-eae5-11ea-86da-bd87745ca1fb.png)
+<br><br>
+### ▶️ Compare meds <br>
+You can navigate to this feature from the main page navbar and then you can compare two of your medicines that you are confused in and you will get the results according to other reviews. The result will be in two forms, side by side **Graph** and **Comparison Table** for the full detailed comparison of both in tabular form. <br>
+
+![Annotation 2020-09-03 180959](https://user-images.githubusercontent.com/44469087/92116890-0d689080-ee12-11ea-92dc-ed324bec3d1f.png)
+
+### ▶️ History <br>
+From here you can get all the records for your previous search with date and time, name of medicine, and disease.
+You can also see the result again by clicking the **See results** Button.
+
+###  ▶️ Monitoring/Query <br>
+You can see what's going on recently or the history of medicine. For the section, the application uses NewsAPI and tweets to show the sentiment of the user changing with the time for that medicine or disease.<br>
+Available with both  **⌨️Typing Search** &  **🎙️Voice Search**
+
+![Annotation 2020-09-03 181155](https://user-images.githubusercontent.com/44469087/92116657-bcf13300-ee11-11ea-8ec2-7c65519efe95.png)
+
+###  ▶️ COVID Chatbot <br>
+A chatbot to solve all your queries regarding the Covid-19 situation and tries to help you in the best possible ways.
+The chatbot works on both **Messenger** and **Telegram**.
+![Annotation 2020-09-06 143520](https://user-images.githubusercontent.com/44469087/92323387-e9958c80-f055-11ea-94e7-6ddb5ffb71b8.png)
+
+###  ▶️ Speed <br>
+Analyzing 2 million of the dataset in just some seconds. That makes it more User-Friendly and Time-Efficient.
+
+<hr><hr>
+
+# Architecture ⚙️ <br>
+This is the overall architecture of our application. on the left side, the red-colored sections are those with which the user interacts and on it right the brief internal working of our application is shown.<br> 
+![WhatsApp Image 2020-09-06 at 3 18 46 PM](https://user-images.githubusercontent.com/44469087/92323221-91aa5600-f054-11ea-94fa-3eef580ca462.jpeg)
+<hr><hr>
+
+# Details of NLP Model 📚 <br>
+
+### ▶️ About Model <br>
+Sentiment Analysis is used on the pre-saved dataset (over 2 million) and the fixed data that is being Scraped from Twitter and Drug.com. Then data is broken into tokens and Using this dataset we train our model over **Wit.ai**, Wit.ai Speech API is also used to make our application voice interactive.  <br>
+### ▶️ Model's Accuracy <br>
+On running our Model on a pre-stored dataset we get the accuracy of about 85% and when the twitter and drug.com dataset is added accuracy increases to 87.05 %.<br>
+### ▶️ Future Updated <br>
+This project has a very wide area to which we can explore and we can add many new features in it. For the future, We are thinking of adding a feature to suggest the best Doctors and Best hospitals for the searched medicine. and it can be according to the city of the user and also best in his country By getting user location and Analyzing data from different platforms about the best doctors and hospitals for that disease. <hr><hr>
+
+> # Technology Used 💻<br>
+
+> - <b>Frontend & UI :-</b>    ``` HTML ```     ``` CSS ```   ``` Bootstrap ```    ``` Javascript ```
+
+> - <b>Backend :-</b> ``` Flask ```
+
+> - <b>Database :-</b> ``` PostgreSQL ```
+
+> - <b>Voice intraction :-</b> ``` Annayang.js ```
+
+> - <b>Authentication :-</b> ``` Google Auth0 ```
+
+> - <b>Model :-</b> ``` Wit.ai ```
+
+> - <b>Support for Model :-</b>   ``` NLP ```   ``` UCI ```
+
+<hr><hr>
+
+## Overview 💡 <br>
+☐ It helps the user to better understand the medicine he is using and provide helpful feedback.<br>
+☐ Deep sentiment analysis is performed for over 2 million datasets.<br>
+☐ It has the ability to suggest best-rated medicines for that disease.<br>
+☐ It also gives the list of other diseases on which the same medicine works.<br>
+☐ Both graphical and statistical representations of data for better understanding.<br>
+☐ Feature to download your result in Pdf softcopy.<br>
+☐ The history section for all your searched results so you can check anytime and revisit.<br>
+☐ Compare the Meds section for knowing the best medicine B/W 2 if you are confused.
+<hr><hr>
+
+# Fun Fact 👻<br>
+🐬 Our Chotu was pretty hard to handle because he has a great sense of Humor. <br>
+🐬 Designing a logo took a lot more time than the development of the platform. 😝
+<hr><hr>
---
+++ HackthonFB/app.py
@@ -0,0 +1,486 @@
+from flask import Flask,render_template,url_for,request,redirect
+import pandas as pd
+from statistics import mean
+from flask_dance.contrib.google import make_google_blueprint, google
+import os
+from wit import Wit
+from io import StringIO
+
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from newsapi import NewsApiClient
+
+import tweepy
+from tweepy import OAuthHandler
+ 
+# Your Twittter App Credentials
+consumer_key = "IaP4pd9lH0TEkHywsdiYqEMMb"
+consumer_secret = "qvM0Q98xsjsspbO9ayVeiWjkaH4rRSMiRffYctZxeY7bGiKNR8"
+access_token = "1300109435822505985-4xuf3g1dvlvfz4Dgr41Wnx4vuxjUA4"
+access_token_secret = "P3qhYgLemyMJXZgHN7FTrLDg1ShzNfDvDwbBRmTH9BJDW"
+ 
+# Calling API
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+ 
+
+
+'''
+# data preprocessing part, adding for understanding ... not your job
+# from bs4 import BeautifulSoup
+# import nltk
+# from nltk.corpus import stopwords
+# from nltk.stem.snowball import SnowballStemmer
+# \r\n : we need to convert html grammer
+# ... , ' : deal with not alphabet
+# this is most shit part, but have to do it
+# stemmer = SnowballStemmer('english')
+# def review_to_words(raw_review):
+#     # 1. Delete HTML 
+#     review_text = BeautifulSoup(raw_review, 'html.parser').get_text()
+#     # 2. Make a space
+#     letters_only = re.sub('[^a-zA-Z]', ' ', review_text)
+#     # 3. lower letters
+#     words = letters_only.lower().split()
+#     # 5. Stopwords 
+#     meaningful_words = [w for w in words if not w in stops]
+#     # 6. Stemming
+#     stemming_words = [stemmer.stem(w) for w in meaningful_words]
+#     # 7. space join words
+#     return( ' '.join(stemming_words))
+# Almost died in writing this line ;)
+# %time df_train['review'] = df_train['review'].apply(review_to_words)
+# CPU times: user 5min 33s, sys: 1.66 s, total: 5min 35s
+# Wall time: 5min 40s
+'''
+
+
+app=Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+#newsapi
+newsapi = NewsApiClient(api_key='d81b895c895046279bfbbcad8be288ac')
+
+# /v2/top-headlines
+
+
+
+class History(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	email = db.Column(db.String(200), nullable=False)
+	name = db.Column(db.String(200), default="Sundaram Dubey")
+	condition = db.Column(db.String(200), default="Heart Atack")
+	medicine = db.Column(db.String(200), default="apirin")
+	date_create = db.Column(db.DateTime, default=datetime.utcnow)
+
+	def __repr__(self):
+		return "<task %r> " % self.id
+
+db.create_all()
+
+client = Wit("Y6XVDB4EY4K7RC2QMWAWXQSAZDOR2NOM")
+
+app.secret_key = "Remember hope is a good thing maybe the best of the things and no good thing ever dies"
+app.config["GOOGLE_OAUTH_CLIENT_ID"] = "751522856838-vm3cp81av9cbi83ucp454vumhv4ud3bg.apps.googleusercontent.com"
+app.config["GOOGLE_OAUTH_CLIENT_SECRET"] = "D3l7vOlgoKPsB_MtgGFWmJcm"
+google_bp = make_google_blueprint(scope=["profile", "email"])
+app.register_blueprint(google_bp, url_prefix="/login")
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
+
+# https://drive.google.com/file/d/11w3B_ni4u2LvK4OPQ07cS3yeCgIl7re5/view?usp=sharing
+## you won't get it, so don't try to understand, it's complicated..
+
+
+# url1 = 'https://drive.google.com/file/d/11w3B_ni4u2LvK4OPQ07cS3yeCgIl7re5/view?usp=sharing'
+# path1 = 'https://drive.google.com/uc?export=download&id='+url1.split('/')[-2]
+
+# url2 = 'https://drive.google.com/file/d/18oXt6odrqNYHGL6cCWltX0DKFahes_gu/view?usp=sharing'
+# path2 = 'https://drive.google.com/uc?export=download&id='+url2.split('/')[-2]
+
+
+df_train = pd.read_csv("C:\\Users\\SARTHAK SINGH\\Desktop\\Git\\drugsComTrain_raw.csv")
+df_test = pd.read_csv("C:\\Users\\SARTHAK SINGH\\Desktop\Git\\drugsComTest_raw.csv")
+
+## don't mess with the following code, out of ur reach
+df_all = pd.concat([df_train,df_test])
+
+## Not proud of myself after doing this, but it worked..
+drug = list(df_all["drugName"])
+disease = list(df_all["condition"])
+rating = list(df_all["rating"])
+review = list(df_all["review"])
+userid = list(df_all["uniqueID"])
+date = list(df_all["date"])
+usefulCount = list(df_all["usefulCount"])
+# l = df_all["date"].dt.year
+# df_all["year"] = l
+# years = list(df_all["year"])
+
+## removing "90 useRS </span> found it useful on demand of sarthak singh.."
+#count = 0
+for i in range(len(disease)):
+    if("span" in str(disease[i])):
+        #print(dieses[i])
+        disease[i] = "nothing"
+        rating[i] = 0
+        review[i] = "Not a review to view"
+
+
+@app.route('/', methods =['GET', 'POST'])
+def home():
+	if(request.method == 'GET'):
+		is_active = True
+		if not google.authorized:
+			is_active = False
+		try:
+			resp = google.get("/oauth2/v1/userinfo")
+			assert resp.ok, resp.text
+		except:  # or maybe any OAuth2Error
+			return render_template("index.html", is_active = is_active)
+
+		redirect_url = url_for("google.login")
+		resp = google.get("/oauth2/v1/userinfo")
+		name=resp.json()["name"]
+		picture=resp.json()["picture"]
+		return render_template("index.html", redirect_url= redirect_url, is_active = is_active, name = name, picture=picture)
+	else:
+		if not google.authorized:
+			return redirect(url_for("google.login"))
+		resp = google.get("/oauth2/v1/userinfo")
+		name=resp.json()["name"]
+		email=resp.json()["email"]
+		picture = resp.json()["picture"]
+		condition = request.form['condition']
+		meds = request.form['drug']
+		new_history = History(email=email, name=name, condition=condition, medicine=meds)
+		try:
+			db.session.add(new_history)
+			db.session.commit()
+		except:
+			return "there was an error in adding your task"
+
+		search_disease = condition
+		search_medicine = meds
+		suitable_rating = []
+		suitable_other_meds = []
+		suitable_other_desease = []
+		reviews = []
+		review_with_rating = []
+		negative_sentiments = 0
+		positive_sentiments = 0
+		neutral_sentiments = 0
+		suitable_other_meds_name=[]
+		suitable_other_meds_rating=[]
+		for i in range(len(drug)):
+			if(str(condition).lower() == str(disease[i]).lower() and str(meds).lower() == str(drug[i]).lower()):
+				suitable_rating.append(rating[i])
+				review_with_rating.append((rating[i], review[i]))
+				review_msg = client.message(review[i][:280])
+				sentiment = ""
+				score = 1
+				#print(review_msg["traits"]['wit$sentiment'])
+				if(review_msg["traits"] != {}):
+					if(review_msg["traits"]["wit$sentiment"][0]["value"] == 'positive'):
+						sentiment = "Positive"
+						positive_sentiments += 1
+						score = review_msg["traits"]["wit$sentiment"][0]["confidence"]
+					if(review_msg["traits"]["wit$sentiment"][0]["value"] == 'negative'):
+						sentiment = "Negative"
+						negative_sentiments +=1
+						score = review_msg["traits"]["wit$sentiment"][0]["confidence"] * (-1)
+					if(review_msg["traits"]["wit$sentiment"][0]["value"] == 'neutral'):
+						sentiment = "Neutral"
+						neutral_sentiments +=1
+						score = review_msg["traits"]["wit$sentiment"][0]["confidence"] * 0
+					reviews.append((review[i],date[i], sentiment,score))
+
+			if(str(condition).lower() == str(disease[i]).lower() and rating[i] > 7):
+				suitable_other_meds_name.append(drug[i])
+				suitable_other_meds_rating.append(rating[i])
+				suitable_other_meds.append((rating[i], drug[i]))
+			if(str(meds).lower() == str(drug[i]).lower() and rating[i] >7):
+				suitable_other_desease.append((int(rating[i]),disease[i]))
+		if(len(suitable_rating) > 0):
+			suitable_rating = mean(suitable_rating)
+			suitable_rating = round(suitable_rating, 3)
+		else:
+			suitable_rating = 0
+		suitable_other_desease = list(set(suitable_other_desease))
+		suitable_other_meds = list(set(suitable_other_meds))
+	#	suitable_other_desease.sort()
+	#	suitable_other_meds.sort()
+		suitable_other_desease = suitable_other_desease[::-1]
+		suitable_other_meds =suitable_other_meds[::-1]
+		if(len(suitable_other_desease) > 10):
+			suitable_other_desease = suitable_other_desease[:10]
+		if(len(suitable_other_meds) > 10):
+			suitable_other_meds = suitable_other_meds[::10]
+		review_with_rating.sort()
+		most_positive_review = "None"
+		most_negative_review="None"
+		if(len(review_with_rating) > 0):
+			most_negative_review = review_with_rating[0][1]
+			most_positive_review = review_with_rating[-1][1]
+		total_sentiments = neutral_sentiments +positive_sentiments+negative_sentiments
+		if(total_sentiments > 0):
+			positive_sentiments = (positive_sentiments / total_sentiments)*100
+			negative_sentiments = (negative_sentiments/ total_sentiments)*100
+			neutral_sentiments = (neutral_sentiments/total_sentiments) * 100
+			positive_sentiments= round(positive_sentiments,2)
+			neutral_sentiments = round(neutral_sentiments,2)
+			negative_sentiments = round(negative_sentiments,2)
+
+		p=[positive_sentiments,negative_sentiments,neutral_sentiments]
+		if len(suitable_other_meds_name) >5 :
+			suitable_other_meds_name=suitable_other_meds_name[:5]
+		if len(suitable_other_meds_rating) >5 :
+			suitable_other_meds_rating=suitable_other_meds_rating[:5]
+		# print(suitable_other_meds_name)
+		# print(suitable_other_meds_rating)
+		return render_template("result.html",
+                           total_sentiments=total_sentiments, 
+                           search_disease = search_disease, 
+                           most_positive_review=most_positive_review,
+                           most_negative_review=most_negative_review,
+                           reviews=reviews,
+                           suitable_other_meds=suitable_other_meds,
+                           suitable_other_desease=suitable_other_desease, 
+                           suitable_rating=suitable_rating,
+                           search_medicine=search_medicine,
+                           positive_sentiments=positive_sentiments,
+                           negative_sentiments= negative_sentiments,
+                           neutral_sentiments=neutral_sentiments,
+                           email = email,
+                           picture = picture,
+						   p=p,
+                           suitable_other_meds_name=suitable_other_meds_name,
+                           suitable_other_meds_rating=suitable_other_meds_rating)
+
+@app.route("/history")
+def history():
+	if not google.authorized:
+		return redirect(url_for("google.login"))
+	resp = google.get("/oauth2/v1/userinfo")
+	name=resp.json()["name"]
+	email=resp.json()["email"]
+	picture = resp.json()["picture"]
+	tasks = History.query.filter_by(email=email)
+	tasks = sorted(tasks, key = lambda i: i.date_create, reverse=True)
+	return render_template("history.html", tasks = tasks, picture = picture)
+
+
+@app.route("/profile")
+def profile():
+	if not google.authorized:
+		return redirect(url_for("google.login"))
+	resp = google.get("/oauth2/v1/userinfo")
+	name=resp.json()["name"]
+	email=resp.json()["email"]
+	picture = resp.json()["picture"]
+	return render_template('profile.html', name=name, email=email, picture=picture)
+
+
+@app.route("/chatbot")
+def chatbot():
+	return render_template('chatbot.html')
+
+@app.route("/image")
+def image():
+	return render_template('image.html')
+
+"""
+@app.route("/explore",methods=['GET','POST'])
+def  explore():
+	if request.method == 'POST':
+		search_word=request.form['search_query']
+		print(search_word)
+		top_headlines = newsapi.get_everything(q=search_word,sort_by='relevancy',           
+                                          language='en',)
+		count_of_headlines=top_headlines['totalResults']
+		count_of_headlines=int(count_of_headlines)
+		title_list=[]
+		description_list=[]
+		urls_headlines=[]
+		for headline in top_headlines['articles']:
+			title_list.append(headline['title'])
+			description_list.append(headline['description'])
+			urls_headlines.append(headline['url'])
+		total_elements=len(title_list)
+		print(total_elements)
+		
+		return render_template('explore.html',title_list=title_list,description_list=description_list,
+			urls_headlines=urls_headlines,total_elements=total_elements)
+	else:
+		return render_template("explore.html",nm="saurabh")
+			
+"""
+
+
+import datetime
+import preprocessor
+@app.route("/explore",methods=['GET','POST'])
+def  explore():
+    if request.method == 'POST':
+        search_word=request.form['search_query']
+        startfrom = request.form['startfrom']
+        startfrom = str(startfrom)
+        startfrom_twitter = startfrom
+        entered_date = startfrom.split("-")
+        current_time = datetime.datetime.now()
+        print(startfrom)
+        if(int(entered_date[0])!= int(current_time.year) or int(entered_date[1]) != int(current_time.month)):
+            startfrom = str(current_time.year) + "-0" + str(current_time.month) + "-" + "01"
+        top_headlines = newsapi.get_everything(q=search_word,sort_by='relevancy',           
+                                language='en',from_param=str(startfrom))
+        count_of_headlines=top_headlines['totalResults']
+        count_of_headlines=int(count_of_headlines)
+        title_list=[]
+        description_list=[]
+        urls_headlines=[]
+        keyword = search_word
+        ##twitter tweets
+        tweets = api.search(keyword, count=50, lang='en', exclude='retweets',tweet_mode='extended', since=startfrom_twitter)
+        tweet_sentiments = []
+        demo2 = []
+        demo1 = []
+        pos_rev=0
+        neg_rev=0
+        neu_rev=0
+        for i in tweets:
+            processed_tweet = preprocessor.clean(i.full_text)
+            review_msg = client.message(processed_tweet[:280]) 
+            if(review_msg["traits"] != {}):
+                #demo1.append(int(str(i.created_at)[9:11]))
+                tweet_sentiments.append([processed_tweet, review_msg["traits"]["wit$sentiment"][0]["value"]])
+                if(review_msg["traits"]["wit$sentiment"][0]["value"] == "positive"):
+                    demo2.append(1)
+                    pos_rev+=1
+                elif(review_msg["traits"]["wit$sentiment"][0]["value"] == "negative"):
+                    demo2.append(-1)
+                    neg_rev+=1
+                else:
+                    demo2.append(0)
+                    neu_rev+=1
+        for headline in top_headlines['articles']:
+            title_list.append(headline['title'])
+            description_list.append(headline['description'])
+            urls_headlines.append(headline['url'])
+
+
+        total_elements=[]
+        for i in range(len(title_list)):
+            total_elements.append(i)
+        print(total_elements)
+        #x axis -> time -> demo1
+        demo1=[i + 1 for i in range(len(demo2))]
+        #y axis -> rating (1->pos , 0->neutral , -1->negative) -> demo2
+       # demo2=[1,0,-1,0,0,-1]
+        #size of x axis
+        size_demo=[i for i in range(len(demo1))] #store 0 to len(demo1)-1 in size_demo
+        pos_rev=round((pos_rev*100)/len(size_demo),2)
+        neg_rev=round((neg_rev*100)/len(size_demo),2)
+        neu_rev=round((neu_rev*100)/len(size_demo),2)
+        rev_list_precent=[pos_rev,neg_rev,neu_rev]
+
+
+
+        return render_template('explore.html',title_list=title_list,description_list=description_list,
+        urls_headlines=urls_headlines,total_elements=total_elements, tweets = tweets, tweet_sentiments=tweet_sentiments,demo1=demo1,size_demo=size_demo,demo2=demo2,rev_list_precent=rev_list_precent)
+
+    else:
+        return render_template("explore.html")
+
+
+
+
+
+
+
+
+
+@app.route("/compare", methods =['GET', 'POST'])
+def compare():
+	show_graph = False
+	if(request.method == 'GET'):
+		return render_template('compare.html', show=show_graph)
+	else:
+		show_graph = True
+		med1 = request.form["med1"]
+		med2 = request.form["med2"]
+		positive1 = 0
+		negative1 = 0
+		neutral1 = 0
+		positive2 = 0
+		negative2 = 0
+		neutral2 = 0
+		usefulCount1 = 0
+		usefulCount2 = 0
+		most_positive_review1 = "None"
+		most_negative_review1 = "None"
+		most_positive_review2 = "None"
+		most_negative_review2 = "None"
+		side_effect1 ="everything"
+		side_effect2 = "sideefft hi side effect"
+		max_rating_1 = 0
+		max_rating_2 = 0
+		for i in range(len(drug)):
+			if(drug[i] == med1):
+				review_msg = client.message(review[i][:280])
+				usefulCount1 += usefulCount[i]
+				if(rating[i] > max_rating_1):
+					max_rating_1 = rating[i]
+				if(review_msg["traits"] != {}):
+					if(review_msg["traits"]["wit$sentiment"][0]["value"] == 'positive'):
+						positive1 += 1
+						most_positive_review1 = review[i]
+						score = review_msg["traits"]["wit$sentiment"][0]["confidence"]
+					if(review_msg["traits"]["wit$sentiment"][0]["value"] == 'negative'):
+						negative1 +=1
+						most_negative_review1 = review[i]
+						score = review_msg["traits"]["wit$sentiment"][0]["confidence"] * (-1)
+					if(review_msg["traits"]["wit$sentiment"][0]["value"] == 'neutral'):
+						neutral1 +=1
+						score = review_msg["traits"]["wit$sentiment"][0]["confidence"] * 0
+			if(drug[i] == med2):
+				if(rating[i] > max_rating_2):
+					max_rating_2 = rating[i]
+				review_msg = client.message(review[i][:280])
+				usefulCount2 += usefulCount[i]
+				if(review_msg["traits"] != {}):
+					if(review_msg["traits"]["wit$sentiment"][0]["value"] == 'positive'):
+						positive2 += 1
+						most_positive_review2 = review[i]
+						score = review_msg["traits"]["wit$sentiment"][0]["confidence"]
+					if(review_msg["traits"]["wit$sentiment"][0]["value"] == 'negative'):
+						negative2 +=1
+						most_negative_review2 = review[i]
+						score = review_msg["traits"]["wit$sentiment"][0]["confidence"] * (-1)
+					if(review_msg["traits"]["wit$sentiment"][0]["value"] == 'neutral'):
+						neutral2 +=1
+						score = review_msg["traits"]["wit$sentiment"][0]["confidence"] * 0
+			
+			med1_data = [positive1, negative1, neutral1]
+			med2_data = [positive2, negative2, neutral2]
+
+		return render_template('compare.html',
+							show=show_graph,
+							med1_data = med1_data,
+							med2_data = med2_data,
+							med1= med1, med2=med2,
+							most_negative_review1 = most_negative_review1,
+							most_positive_review1=most_positive_review1,
+							most_positive_review2=most_positive_review2,
+							most_negative_review2=most_negative_review2,
+							usefulCount1=usefulCount1,
+							usefulCount2=usefulCount2,
+							max_rating_1=max_rating_1,
+							max_rating_2=max_rating_2)
+
+if __name__=='__main__':
+	app.run(debug=True)    
+
+# I AM IRON-MAN
